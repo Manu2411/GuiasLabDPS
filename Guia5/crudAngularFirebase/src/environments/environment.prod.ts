75e50dc654ec3cb87f629968148ea0709b3f6b57
Manu2411 <manuhurtado_24@hotmail.com>
2020-09-14 16:30:36 -0600

    Subiendo ejemplo de la guía 5 con base de datos en Firebase

---
+++ Guia5/crudAngularFirebase/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    apiKey: "AIzaSyBde0Zz3sN1SVGoJQxVPC_Vo6w18nMlCdY",
+    authDomain: "crudangularfirebase-3e8b8.firebaseapp.com",
+    databaseURL: "https://crudangularfirebase-3e8b8.firebaseio.com",
+    projectId: "crudangularfirebase-3e8b8",
+    storageBucket: "crudangularfirebase-3e8b8.appspot.com",
+    messagingSenderId: "446608183972",
+    appId: "1:446608183972:web:40b334ef73f2f465224c31",
+    measurementId: "G-56Z3FH2F8E"
+  }
+};
---
+++ Guia5/crudAngularFirebase/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    apiKey: "AIzaSyBde0Zz3sN1SVGoJQxVPC_Vo6w18nMlCdY",
+    authDomain: "crudangularfirebase-3e8b8.firebaseapp.com",
+    databaseURL: "https://crudangularfirebase-3e8b8.firebaseio.com",
+    projectId: "crudangularfirebase-3e8b8",
+    storageBucket: "crudangularfirebase-3e8b8.appspot.com",
+    messagingSenderId: "446608183972",
+    appId: "1:446608183972:web:40b334ef73f2f465224c31",
+    measurementId: "G-56Z3FH2F8E"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
