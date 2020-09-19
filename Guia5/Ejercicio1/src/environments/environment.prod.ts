a23b141eaedb100411eea4f650f9a53dc380c177
Manu2411 <manuhurtado_24@hotmail.com>
2020-09-19 16:46:11 -0600

    Ejercicio 1 Guia 5

---
+++ Guia5/Ejercicio1/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+  apiKey: "AIzaSyCZc45IqMjczFjnyL0y5utKmOFsdQi5aXU",
+  authDomain: "crudschool-71414.firebaseapp.com",
+  databaseURL: "https://crudschool-71414.firebaseio.com",
+  projectId: "crudschool-71414",
+  storageBucket: "crudschool-71414.appspot.com",
+  messagingSenderId: "419621584535",
+  appId: "1:419621584535:web:6881069bc63e2c2a6973d2",
+  measurementId: "G-LHDJE4P1YZ"
+}
+};
